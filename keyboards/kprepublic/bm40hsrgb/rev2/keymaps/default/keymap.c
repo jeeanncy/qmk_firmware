eb13922e2910d45988ea606d4692557daf4569f2
Jean Ccana <jeanccana@MacBook-Pro-de-Jean.local>
2022-11-27 23:56:08 -0500

    changues

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/default/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/default/keymap.c
@@ -75,7 +75,7 @@ void setGameRGB(int layer) {
       }
 };
 
-#define CAPSFN LT(_FN, KC_CAPS)
+#define ESCFN LT(_FN, KC_ESC)
 #define LOWER LT(_LOWER, KC_ENT)
 #define SPCHOM LT(_HOME, KC_SPC)
 #define RAISE LT(_RAISE, KC_BSPC)
@@ -84,13 +84,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_planck_mit(
         /*  ,----------------------------------------------------------------------------------------------------------------.*/
-               KC_TAB  ,   KC_Q  ,   KC_W  ,  KC_E   , KC_R  ,  KC_T  , KC_Y , KC_U  ,   KC_I  ,  KC_O   ,  KC_P   , KC_ESC  ,
+               KC_TAB  ,   KC_Q  ,   KC_W  ,  KC_E   , KC_R  ,  KC_T  , KC_Y , KC_U  ,   KC_I  ,  KC_O   ,  KC_P   ,  ESCFN  ,
         /*  |----------+---------+---------+---------+-------+--------+------+-------+---------+---------+---------+---------| */
-               KC_LCTL ,   KC_A  ,   KC_S  ,  KC_D   , KC_F  ,  KC_G  , KC_H , KC_J  ,   KC_K  ,  KC_L   , KC_SCLN , KC_QUOT ,
+               KC_LGUI ,   KC_A  ,   KC_S  ,  KC_D   , KC_F  ,  KC_G  , KC_H , KC_J  ,   KC_K  ,  KC_L   , KC_SCLN , KC_QUOT ,
         /*  |----------+---------+---------+--- -----+-------+--------+------+-------+---------+---------+---------+---------| */
                KC_LSFT ,   KC_Z  ,   KC_X  ,   KC_C  , KC_V  ,  KC_B  , KC_N , KC_M  , KC_COMM ,  KC_DOT , KC_SLSH , KC_RSFT ,
         /*  |----------+---------+---------+---------+-------+--------+------+-------+---------+---------+---------+---------| */
-               CAPSFN  , KC_LCTL , KC_LGUI , KC_LALT , LOWER ,     SPCHOM    , RAISE , KC_MPRV , KC_VOLD , KC_VOLU , KC_MNXT ),
+               KC_CAPS , KC_LCTL , KC_LALT , KC_LGUI , LOWER ,     SPCHOM    , RAISE , KC_ALGR , KC_VOLD , KC_VOLU , KC_MPLY ),
         /* `-----------------------------------------------------------------------------------------------------------------' */
 
     [1] = LAYOUT_planck_mit(
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /*  ,-------------------------------------------------------------------------------------------------------------------------.*/
                XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_HOME ,  KC_UP  ,  KC_END  , XXXXXXX , XXXXXXX ,
         /*  |----------+---------+---------+---------+---------+---------+---------+---------+---------+----------+---------+---------| */
-               XXXXXXX , KC_LSFT , KC_LGUI , KC_LALT , KC_LCTL , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_RIGHT , XXXXXXX , XXXXXXX ,
+               XXXXXXX , KC_LSFT , KC_LCTL , KC_LALT , KC_LGUI , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_RIGHT , XXXXXXX , XXXXXXX ,
         /*  |----------+---------+---------+---------+---------+---------+---------+---------+---------+----------+---------+---------| */
                XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  , XXXXXXX , XXXXXXX ,
         /*  |----------+---------+---------+---------+---------+---------+---------+---------+---------+----------+---------+---------| */
@@ -139,11 +139,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [5] = LAYOUT_planck_mit(
         /*  ,------------------------------------------------------------------------------------------------------------------------.*/
-               XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  , XXXXXXX ,
+               XXXXXXX ,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  KC_F5  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
         /*  |----------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------| */
-               XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,  KC_F5  ,  KC_F6  ,  KC_F7  ,  KC_F8  , XXXXXXX ,
+               XXXXXXX ,  KC_F6  ,  KC_F7  ,  KC_F8  ,  KC_F9  ,  KC_F10 , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
         /*  |----------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------| */
-               XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,  KC_F9  ,  KC_F10 ,  KC_F11 ,  KC_F12 , XXXXXXX ,
+               XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,  KC_F11 ,  KC_F12 , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
         /*  |----------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------| */
                XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,     XXXXXXX       , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ),
         /* `-------------------------------------------------------------------------------------------------------------------------' */
