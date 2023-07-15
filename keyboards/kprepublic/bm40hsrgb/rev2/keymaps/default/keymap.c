80da7999e01451000492e24e747ca08e95d4b47f
jeeanncy <39495973+jeeanncy@users.noreply.github.com>
2023-07-15 06:22:49 +0000

    Changues keys

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/default/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/default/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /*  |----------+---------+---------+--- -----+-------+--------+------+-------+---------+---------+---------+---------| */
                KC_LSFT ,   KC_Z  ,   KC_X  ,  KC_C   , KC_V  ,  KC_B  , KC_N , KC_M  , KC_COMM ,  KC_DOT , KC_SLSH , KC_RSFT ,
         /*  |----------+---------+---------+---------+-------+--------+------+-------+---------+---------+---------+---------| */
-               KC_LCTL , KC_LGUI , KC_LALT ,   FN    , LOWER ,     SPCHOM    , RAISE , KC_ALGR , KC_VOLD , KC_VOLU , KC_MPLY ),
+                 FN    , KC_LCTL , KC_LGUI , KC_LALT , LOWER ,     SPCHOM    , RAISE , KC_ALGR , KC_VOLD , KC_VOLU , KC_MPLY ),
         /* `-----------------------------------------------------------------------------------------------------------------' */
 
     [1] = LAYOUT_planck_mit(
